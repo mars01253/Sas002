522d93cf8b02963787cf480b62c5242293f63cfa
mouad toto <mauadtoto001@gmail.com>
2023-09-13 09:47:18 +0100

    created new C file and wrote some code

---
+++ Sasproject.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+    int age = 22;
+    char name []="mouad";
+    float height = 1.77;
+    printf(" hello\n my name is %s im %d years old\n And %.2f tall",name,age,height);
+    return 0 ;
+
+}
