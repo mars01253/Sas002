8af623f126ba7f4889facd53f76f02f0bfef4878
mouad toto <mauadtoto001@gmail.com>
2023-09-14 10:01:38 +0100

    added a file

---
+++ assignments.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+int main (){
+/*char nom[10];
+char prenom[10];
+int age ; 
+char sexe [10];
+int numero  [10];
+ 
+printf("entrer votre nom:");
+scanf("%s",&nom);
+printf("entrer votre prenom:");
+scanf("%s",&prenom);
+printf("entrer votre age :");
+scanf("%d",&age);
+printf("votre sexe:");
+scanf("%s",&sexe);
+printf("votre numero:");
+scanf("%d", &numero);
+if (numero>=10)
+{
+    printf("error a number cannot exceed ten numbers");
+}
+
+
+
+return 0 ;
+*/
+/*double F ,C;
+printf("entrer la chaleur en F :");
+scanf("%lf",&F);
+C=(F-32)/1.8;
+printf("%.2lf",C);
+if (C<=15)
+{
+    printf(" tres froid");
+}
+else if (C>=15 && C<=25)
+{
+     printf(" froid");
+    
+}
+else if (C>=26&& C <=35)
+{
+     printf(" chaud");
+}
+else{
+     printf(" tres chaud");
+}
+
+return 0 ;
+*/
+/*int a , b ;
+float result;
+printf("entrer a :");
+scanf("%d",&a);
+printf("entrer b :");
+scanf("%d",&b);
+result = a*b ;
+printf("%.2f" , result);
+return 0 ;
+*/
+
+
+/*float a , b , c , result ;
+printf("entrer a :");
+scanf("%f", &a);
+printf("entrer b :");
+scanf("%f", &b);
+printf("entrer c :");
+scanf("%f", &c); 
+
+result = (a+b+c)/3;
+printf("%f", result);
+return 0 ;
+*/
+
+
+
+
+
+
+}
